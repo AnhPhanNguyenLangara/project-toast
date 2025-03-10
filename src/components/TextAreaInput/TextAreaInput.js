85f2e5ebd223cedb0bc33abdf23e0dc35577fc9f
AnhPhanNguyenLangara <131826328+AnhPhanNguyenLangara@users.noreply.github.com>
2025-03-10 16:05:17 -0700

    Add logic for TextAreaInput component

--- src/components/TextAreaInput/TextAreaInput.js
+++ src/components/TextAreaInput/TextAreaInput.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React from "react";
+import styles from "../ToastPlayground/ToastPlayground.module.css";
 
-function TextAreaInput() {
-  return <div></div>;
+function TextAreaInput({ label, content, setContent }) {
+  const inputId = React.useId();
+  return (
+    <div className={styles.row}>
+      <label
+        htmlFor={inputId}
+        className={styles.label}
+        style={{ alignSelf: "baseline" }}
+      >
+        {label}
+      </label>
+      <div className={styles.inputWrapper}>
+        <textarea
+          value={content}
+          id={inputId}
+          className={styles.messageInput}
+          onChange={(e) => setContent(e.target.value)}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default TextAreaInput;
