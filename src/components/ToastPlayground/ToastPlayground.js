f1c536924a91833ab642b924df05fac2e82a0157
AnhPhanNguyenLangara <131826328+AnhPhanNguyenLangara@users.noreply.github.com>
2025-03-10 15:56:09 -0700

    Replace HTML with component instance for RadioGroupInput

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import Button from '../Button';
+import Button from "../Button";
 
-import styles from './ToastPlayground.module.css';
+import RadioGroupInput from "../RadioGroupInput";
 
-const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
+import styles from "./ToastPlayground.module.css";
+
+const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
 
 function ToastPlayground() {
+  const [selectedVariant, setSelectedVariant] = React.useState(
+    VARIANT_OPTIONS[0]
+  );
+
   return (
     <div className={styles.wrapper}>
       <header>
@@ -19,7 +25,7 @@ function ToastPlayground() {
           <label
             htmlFor="message"
             className={styles.label}
-            style={{ alignSelf: 'baseline' }}
+            style={{ alignSelf: "baseline" }}
           >
             Message
           </label>
@@ -28,30 +34,16 @@ function ToastPlayground() {
           </div>
         </div>
 
-        <div className={styles.row}>
-          <div className={styles.label}>Variant</div>
-          <div
-            className={`${styles.inputWrapper} ${styles.radioWrapper}`}
-          >
-            <label htmlFor="variant-notice">
-              <input
-                id="variant-notice"
-                type="radio"
-                name="variant"
-                value="notice"
-              />
-              notice
-            </label>
-
-            {/* TODO Other Variant radio buttons here */}
-          </div>
-        </div>
+        <RadioGroupInput
+          label="Variant"
+          values={VARIANT_OPTIONS}
+          selectedValue={selectedVariant}
+          setSelectedValue={setSelectedVariant}
+        ></RadioGroupInput>
 
         <div className={styles.row}>
           <div className={styles.label} />
-          <div
-            className={`${styles.inputWrapper} ${styles.radioWrapper}`}
-          >
+          <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
             <Button>Pop Toast!</Button>
           </div>
         </div>
