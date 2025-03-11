5d363836d8bff1bdddfbf670791afb44b661e379
AnhPhanNguyenLangara <131826328+AnhPhanNguyenLangara@users.noreply.github.com>
2025-03-10 17:02:27 -0700

    Update Toast component logic to be dynamic

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 import {
   AlertOctagon,
   AlertTriangle,
   CheckCircle,
   Info,
   X,
-} from 'react-feather';
+} from "react-feather";
 
-import VisuallyHidden from '../VisuallyHidden';
+import VisuallyHidden from "../VisuallyHidden";
 
-import styles from './Toast.module.css';
+import styles from "./Toast.module.css";
 
 const ICONS_BY_VARIANT = {
   notice: Info,
@@ -18,20 +18,31 @@ const ICONS_BY_VARIANT = {
   error: AlertOctagon,
 };
 
-function Toast() {
+function Toast({ showing = false, setShowing, variant = "notice", content }) {
+  if (!(variant in ICONS_BY_VARIANT)) {
+    throw new Error(
+      `Variant ${variant} not supported. Please use one of the variants ${Object.keys(
+        ICONS_BY_VARIANT
+      )}`
+    );
+  }
+
   return (
-    <div className={`${styles.toast} ${styles.notice}`}>
-      <div className={styles.iconContainer}>
-        <Info size={24} />
+    showing && (
+      <div className={`${styles.toast} ${styles[variant]}`}>
+        <div className={styles.iconContainer}>
+          <Info size={24} />
+        </div>
+        <p className={styles.content}>{content}</p>
+        <button
+          className={styles.closeButton}
+          onClick={() => setShowing(false)}
+        >
+          <X size={24} />
+          <VisuallyHidden>Dismiss message</VisuallyHidden>
+        </button>
       </div>
-      <p className={styles.content}>
-        16 photos have been uploaded
-      </p>
-      <button className={styles.closeButton}>
-        <X size={24} />
-        <VisuallyHidden>Dismiss message</VisuallyHidden>
-      </button>
-    </div>
+    )
   );
 }
 
